ff9f11e39d763f7ba09264829d1d53f9232e7a2c
HyemIin <hyem5019@gmail.com>
2023-10-05 19:24:06 +0900

    Feat: project build

---
+++ src/main/java/com/example/hello_docker_ec2/HelloDockerEc2Application.java
@@ -0,0 +1,13 @@
+package com.example.hello_docker_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDockerEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hello_docker_ec2/HelloDockerEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_docker_ec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
